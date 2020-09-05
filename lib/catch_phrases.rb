1facf329131cfbaa61de6d731bbe6aea66c29d5c
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-05 01:22:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,14 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase_two = "It's Dangerous To Go Alone! Take This."
+  puts phrase_two
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
+end
